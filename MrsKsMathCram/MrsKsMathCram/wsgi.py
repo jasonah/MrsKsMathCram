6c8d58ca0720fbb5b591b6cb67ad6c6fb7fb9a8a
Jason Hartlein <jasonh1028@gmail.com>
2019-08-13 22:18:01 -0500

    changed website structure for future installments.

--- MrsKsMathCram/MrsKsMathCram/wsgi.py
+++ MrsKsMathCram/MrsKsMathCram/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for MrsKsMathCramSite project.
+WSGI config for MrsKsMathCram project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MrsKsMathCramSite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MrsKsMathCram.settings')
 
 application = get_wsgi_application()
---
+++ MrsKsMathCram/MrsKsMathSite/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MrsksmathsiteConfig(AppConfig):
+    name = 'MrsKsMathSite'
