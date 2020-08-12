8ee8d0ca9f017ce1d1575a04925e72b84651817e
KennyKruck <KennyKruckenberg13@gmail.com>
2020-08-11 23:01:25 -0500

    updated requirements.txt

--- MrsKsMathCram_Source/Videos/models.py
+++ MrsKsMathCram_Source/Videos/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 from Memberships.models import Membership
 from django.urls import reverse
+import re
+import vimeo
+
+client = vimeo.VimeoClient(
+  token='101cc19b08e333d78e17fe1c3e8f45d7',
+  key='92bcc63acb140e48a348db92f42ffe2a26168942',
+  secret='clqedoUebehQOvfbBBu58S2XYKMXzP7C2OgZ41icqHesmK47ITyDPDDeQcSF8E6uyUqNoYReA9JnawqFww8PfdDnsdaZfhEnmmtcWUlcTcOtXNrwYKJHtcB3Fgr8hdSU'
+)
+
+# response = client.get(uri)
+# print(response.json())
 
 class Videos(models.Model):
     slug = models.SlugField()
@@ -9,7 +20,7 @@ class Videos(models.Model):
     allowed_memberships = models.ManyToManyField(Membership)
     position = models.IntegerField()
     video_url = models.CharField(max_length=200) #Later on change this to a video hosting service like amazon aws or vimeo
-    thumbnail = models.ImageField()
+    # thumbnail = models.ImageField()
 
     def __str__(self):
         return self.title
@@ -21,3 +32,13 @@ class Videos(models.Model):
     def order_vids(self):
         return self.all().order_by('position') #This should make sure all videos are in order even if we go through and delete some
         
+    def video_id(self):
+        regex = r"vimeo\.com\/(\d+)"
+        test_str = self.video_url
+        matches = re.finditer(regex, test_str, re.IGNORECASE)
+        for matchNum, match in enumerate(matches, start=1):
+            # print ("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()))
+            for groupNum in range(0, len(match.groups())):
+                groupNum = groupNum + 1
+                # print ("Group {groupNum} found at {start}-{end}: {group}".format(groupNum = groupNum, start = match.start(groupNum), end = match.end(groupNum), group = match.group(groupNum)))
+                return match.group(groupNum)
