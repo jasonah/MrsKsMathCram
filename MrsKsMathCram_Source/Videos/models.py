bc88839680a63bf9328a9430eee5764a84217a81
KennyKruck <KennyKruckenberg13@gmail.com>
2020-08-11 23:37:28 -0500

    added bitchin ass thumbnails to the videos_list page

--- MrsKsMathCram_Source/Videos/models.py
+++ MrsKsMathCram_Source/Videos/models.py
@@ -20,7 +20,7 @@ class Videos(models.Model):
     allowed_memberships = models.ManyToManyField(Membership)
     position = models.IntegerField()
     video_url = models.CharField(max_length=200) #Later on change this to a video hosting service like amazon aws or vimeo
-    # thumbnail = models.ImageField()
+    thumbnail = models.ImageField()
 
     def __str__(self):
         return self.title
